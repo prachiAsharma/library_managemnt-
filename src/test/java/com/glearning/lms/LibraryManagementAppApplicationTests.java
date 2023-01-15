392aca1c28609e078e49ca2f752f45bd4c1b8c75
pradeep <pradeep.kumar44@gmail.com>
2023-01-15 17:15:06 +0530

    initial commit for library management solution api

---
+++ src/test/java/com/glearning/lms/LibraryManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glearning.lms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
