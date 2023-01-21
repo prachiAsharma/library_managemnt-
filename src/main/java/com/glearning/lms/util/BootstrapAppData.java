8b7518a30489fcbaec1c005b900cae848067b358
pradeep <pradeep.kumar44@gmail.com>
2023-01-21 18:05:14 +0530

    committing the changes

--- src/main/java/com/glearning/lms/util/BootstrapAppData.java
+++ src/main/java/com/glearning/lms/util/BootstrapAppData.java
@@ -3,6 +3,7 @@ package com.glearning.lms.util;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.glearning.lms.model.Role;
 import com.glearning.lms.model.User;
@@ -18,7 +19,10 @@ public class BootstrapAppData {
 	}
 	
 	@EventListener(ApplicationReadyEvent.class)
+	@Transactional
 	public void insertAppData(ApplicationReadyEvent event) {
+		System.out.println("Application is ready :: ");
+		System.out.println("Inserting the test data :: ");
 		User kiran = new User("kiran", "welcome", "kiran@gmail.com");
 		User vinay = new User("vinay", "welcome", "vinay@gmail.com");
 		
